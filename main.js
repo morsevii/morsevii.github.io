0c8425e4913873c6a66f112ac28c0796d9eca13a
morsevii <33405147+morsevii@users.noreply.github.com>
2021-11-30 07:42:46 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-var LagInt = () => {
+var LagInt = (x,y,xp) => {
+var yp=0;
 
-for(var i=1;i<=n;i++) 	{ 		
+alert(x.length);
+
+for(var i=0;i<x.length;i++) 	{ 		
   var p=1;
-  for(var j=1;j<=n;j++) {
+  for(var j=0;j<y.length;j++) {
     if(i!=j) {
       	p = p* (xp - x[j])/(x[i] - x[j]); 			 
     }
@@ -10,12 +13,16 @@ for(var i=1;i<=n;i++) 	{
 
   yp = yp + p * y[i];
 }
-
+return yp;
 }
 
 $(()=>{
 $('#btnGo').click(()=>{
- alert(123)
+var x=[5,7,11,13,17];
+var y=[150,392,1452,2366,5202];
+var ip = $('#txtIP).val();
+
+ alert(LagInt(x,y,ip))
 });
 
 });
