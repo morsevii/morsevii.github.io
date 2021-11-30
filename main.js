ef4b14e914f31d74cb1604962df5a13512255b33
morsevii <33405147+morsevii@users.noreply.github.com>
2021-11-30 08:21:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -17,10 +17,26 @@ return yp;
 }
 
 $(()=>{
+$('#btnGo').click(()=>
+ var iX = $('#txtX');
+ var iY = $('#txtY');
+var tblData =$('#tblData tbody'); 
+ if(iX.val()!=""&&iY!="")
+   {
+     tbldata.append('<tr><td>'+iX.val()+'</td><td>'+iY.val()+'</td></tr>');
+     
+     iX.val();
+     iY.val();
+   }
+  else{
+   alert("Both X and Y id required!");
+   }
+  });
+  
 $('#btnGo').click(()=>{
 var x=[5,7,11,13,17];
 var y=[150,392,1452,2366,5202];
-var ip = $('#txtIP).val();
+var ip = $('#txtIP').val();
 
  alert(LagInt(x,y,ip,5))
 });
